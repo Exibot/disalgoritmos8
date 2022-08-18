7cf8fd97d52122d7747b5073ceb3ff6dffc2393a
Enrique Altamirano <enrique.altamiros@gmail.com>
2022-08-17 21:47:42 -0300

    se agrega la clase libro, constructor y método insertar

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 def main():
     pass
-    
+    class Libro:
+        def __init__(self, id=None, titulo=None, autor=None, anio=None, izq=None, der=None):
+            self.id = id
+            self.titulo = titulo
+            self.autor = autor
+            self.anio = anio
+            self.izq = izq
+            self.der = der
+        
+        def insertar(self, id, titulo, autor, anio):
+            self.id = id
+            self.titulo = titulo
+            self.autor = autor
+            self.anio = anio
+            
+            
+            
 
 
 if __name__ == '__main__':
